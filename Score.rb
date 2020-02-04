73521da834957d452ae49a0a7a48323c61232cf5
Tiberius88 <Dominocrumble@gmail.com>
2020-02-04 23:02:38 +0800

    Update Score.rb

--- Score.rb
+++ Score.rb
@@ -1,25 +1,26 @@
-VALID_CHOICES = %w(rock paper scissors lizard spock) 
-hash_input = {'l' => 'lizard','r' => 'rock','p' => 'paper','sc' => 'scissors', 'sp' => 'spock'}
-WINNER  = {'lizard' => ['paper', 'spock'] , 'rock' => ['scissors', 'lizard'] , 'paper' => ['rock', 'spock'], 'scissors'  => ['paper', 'lizard'] , 'spock' => ['scissors', 'rock']}
-SYMBOL = { :player => 0, :computer => 0, :draws => 0 }
+VALID_CHOICES = %w(rock paper scissors lizard spock)
+hash_input = { 'l' => 'lizard', 'r' => 'rock', 'p' => 'paper', 'sc' => 'scissors', 'sp' => 'spock' }
+WINNER  = { 'lizard' => ['paper', 'spock'] , 'rock' => ['scissors', 'lizard'], 
+'paper' => ['rock', 'spock'], 'scissors' => ['paper', 'lizard'], 'spock' => ['scissors', 'rock'] }
+SYMBOL = { player: 0, computer: 0, draws: 0 }
 
 if win?(player, computer)
-  SYMBOL[:player] +=1
-elsif win?(computer, player) 
-   SYMBOL[:computer] +=1 
-   else SYMBOL[:draw] +=1
-    end_game
-end   
- 
+  SYMBOL[player] += 1
+elsif win?(computer, player)
+  SYMBOL[computer] += 1
+else
+  SYMBOL[draw] += 1
+  end_game
+end
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
 def win?(player, computer)
-win_on = WINNER[player]
-win_on.include?(computer)
-end 
+  win_on = WINNER[player]
+  win_on.include?(computer)
+end
 
 def display_results(player, computer)
   if win?(player, computer)
@@ -32,29 +33,30 @@ def display_results(player, computer)
 end
 
 loop do
-if SYMBOL[:player] == 5 || SYMBOL[:computer]== 5
-end 
-end 
+  if SYMBOL[player] == 5 || SYMBOL[computer] == 5
+  end
+end
 
 if win?(player, computer)
-prompt("You score 1 point")
-SYMBOL[:player] +=1
+  prompt('You score 1 point')
+  SYMBOL[:player] += 1
 elsif win?(computer, player)
-prompt("Computer score 1 point")
-SYMBOL[:computer] +=1
-  else prompt("Each score 1 point")
-    SYMBOL[:draw] +=1
-    end_game
-    end 
+  prompt('Computer score 1 point')
+  SYMBOL[:computer] += 1
+else
+  prompt('Each score 1 point')
+  SYMBOL[:draw] += 1
+  end_game
+end
 
 loop do
- choice = ' '
- real_choice = nil
+  choice = ' '
+  real_choice = nil
 
-  loop do
-   prompt("Choose one: #{VALID_CHOICES.join(',')}, ==> #{hash_input}")
-   choice = Kernel.gets.chomp
-   real_choice = hash_input.fetch(choice, choice)
+loop do
+  prompt("Choose one: #{VALID_CHOICES.join(',')}, ==> #{hash_input}")
+  choice = Kernel.gets.chomp
+  real_choice = hash_input.fetch(choice, choice)
 
 if VALID_CHOICES.include?(real_choice)
   break
@@ -63,15 +65,14 @@ else
 end
 end
 
-  computer_choice = VALID_CHOICES.sample 
+  computer_choice = VALID_CHOICES.sample
   prompt("You chose: #{real_choice}; Computer chose: #{computer_choice}")
   display_results(real_choice, computer_choice)
   calculate_winner(player_score, computer_score)
 
- 
   prompt('Do you want to play again? Y to do so')
   answer = Kernel.gets.chomp
-  break unless answer.downcase.start_with?('y') 
+  break unless answer.downcase.start_with?('y')
 end
-end
-prompt("Thank you for playing. Good bye!")
+
+prompt('Thank you for playing. Good bye!')
